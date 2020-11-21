8260718328e3cc308f54a59d3bbda4944d56620e
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-11-21 16:04:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas,gameState=0,playerCount,database,form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  
+  canvas = createCanvas(500,500);
+game = new Game();
+game.getState();
+game.start();
+  
+  
+}
+
+function draw(){
+  
+    
+}
+
